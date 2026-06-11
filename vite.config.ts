6abe16c0831abfe95eba96859c682181e0345570
geldopc <geldopc@gmail.com>
2026-06-11 19:18:17 -0300

    Mudando rota

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-	base: process.env.VITE_BASE ?? (command === "build" ? "/me/" : "/"),
+export default defineConfig(() => ({
+	base: "/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
