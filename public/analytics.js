3d4d6318a7553bdd115e2012286d703dfd2cb203
geldopc <geldopc@gmail.com>
2026-06-20 13:00:36 -0300

    feat: add GoatCounter analytics as reusable snippet
    Centraliza o tracking em public/analytics.js para reutilizar nos demais projetos via script src remoto.

---
+++ public/analytics.js
@@ -0,0 +1,10 @@
+(() => {
+	var script = document.createElement("script");
+	script.async = true;
+	script.src = "//gc.zgo.at/count.js";
+	script.setAttribute(
+		"data-goatcounter",
+		"https://geldopc.goatcounter.com/count",
+	);
+	document.body.appendChild(script);
+})();
